65d28f6bf08d46853eecd430a2fbef1b7020d0d3
ChaoTsun-Tsai <90675447+ChaoTsunTsai@users.noreply.github.com>
2024-02-26 11:22:23 +0800

    Add files via upload

---
+++ Step 1/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AD_Manager.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Admin(string role)
+        {
+            if (role == "admin")
+            {
+                return View();
+            }
+            else
+            {
+                // 如果角色不是 admin，可以進行其他處理，例如返回錯誤頁面或重定向到其他頁面
+                return NotFound();
+            }
+        }
+    }
+}
