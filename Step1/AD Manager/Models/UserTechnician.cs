e8c8ab13384b407d6f677bd5b13e7dbbe9596d31
ChaoTsun-Tsai <90675447+ChaoTsunTsai@users.noreply.github.com>
2024-03-01 20:44:56 +0800

    Add files via upload

---
+++ Step1/AD Manager/Models/UserTechnician.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AD_Manager.Models;
+
+public partial class UserTechnician
+{
+    public string Id { get; set; } = null!;
+
+    public string UserName { get; set; } = null!;
+
+    public string UserPassword { get; set; } = null!;
+}
