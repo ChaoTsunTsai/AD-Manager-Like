975d4c3f23b2d355005f7215a4fe6429ef83d352
ChaoTsun-Tsai <90675447+ChaoTsunTsai@users.noreply.github.com>
2024-03-03 22:02:58 +0800

    Add files via upload

---
+++ 20240303/AD Manager/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourNamespace.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Admin()
+        {
+            return View("~/Views/User/Admin.cshtml");
+        }
+
+        public IActionResult Technician()
+        {
+            return View("~/Views/User/Technician.cshtml");
+        }
+
+        public IActionResult CreateSingleUser()
+        {
+            return View("~/Views/User/User Management/CreateSingleUser.cshtml");
+        }
+    }
+}
---
+++ 20240303/AD Manager/Models/UserAdmin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AD_Manager.Models;
+
+public partial class UserAdmin
+{
+    public int Id { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string UserPassword { get; set; } = null!;
+}
